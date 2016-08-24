18691ac7dbe1bc5c5916923f5b8f5c04cac95122
Christopher O'Hara <christopher_ohara@hotmail.com>
2016-08-23 22:01:10 -0500

    Added puma and capistrano for deployment

--- Gemfile
+++ Gemfile
@@ -2,16 +2,11 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'haml'
-gem 'pony'
-gem 'sinatra-captcha'
-gem 'rest-client'
+gem 'puma'
 
 group :development do
-  gem 'better_errors'
-  gem 'binding_of_caller'
-
   gem 'capistrano'
-  gem 'capistrano-bundler'
   gem 'capistrano-rbenv'
-  gem 'capistrano-passenger'
+  gem 'capistrano-bundler'
+  gem 'capistrano3-puma'
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,9 @@
-set :production
-
-server '54.172.137.90', user: 'ubuntu', roles: %w(app web)
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '54.172.137.90', user: 'ubuntu', roles: %w{app web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
